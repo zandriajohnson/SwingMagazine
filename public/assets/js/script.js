dd66e22b31b3351159759382fb3810517f8add81
Xavier Williams <xwq58@mail.missouri.edu>
2018-04-04 21:26:28 -0500

    made a few changes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -49,3 +49,13 @@ function sendEmail(){
 
     window.open('mailto:test@example.com?subject=subject&body=body');
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav-pills") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-pills";
+    }
+}
